e37be94bca38f2e66fdfd802c5f9d76bb6514838
Warren Loy Harper II <warrenharper2@att.net>
2019-07-20 19:55:19 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+["bread","milk","eggs","steak"]shopping_list = 
+shopping_list.include?("bread")
 end
 
 def using_sort(array)
-
+  ["bread","milk","eggs","steak"]
+shopping_list.sort
 end
 
 def using_reverse(array)
-
+  ["bread","milk","eggs","steak"]
+shopping_list.reverse
 end
 
 def using_first(array)
-
+  ["bread","milk","eggs","steak"]
+shopping_list.first
 end
 
 def using_last(array)
-
+  ["bread","milk","eggs","steak"]
+shopping_list.last
 end
 
 def using_size(array)
-
+  ["bread","milk","eggs","steak"]
+shopping_list.size
 end
