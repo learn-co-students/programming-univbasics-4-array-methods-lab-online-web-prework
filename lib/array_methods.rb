45078bb0ad747b54be2030c93abfe291d08d57c2
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-07-23 02:15:06 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,33 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
-
+  
 def using_sort(array)
-
+  number = ["zero", "one", "two", "three"]
+  number.sort
+    => ["zero", "two", "one", "three"]
 end
 
 def using_reverse(array)
-
+  number = ["zero", "one", "two", "three"]
+  number.reverse
+    => ["three", "two", "one", "zero"]
 end
 
 def using_first(array)
-
+  number = ["zero", "one", "two", "three"]
+  number.first
+    => ["zero"]
 end
 
 def using_last(array)
-
+  number = ["zero", "one", "two", "three"]
+  number.last
+    => ["three"]
 end
 
 def using_size(array)
-
+  number = ["zero", "one", "two", "three"]
+  number.size
+    => 3
 end
