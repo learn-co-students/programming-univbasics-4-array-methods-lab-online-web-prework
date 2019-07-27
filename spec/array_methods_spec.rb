5ab00dfd776fd46fbf113e3765e03c739a2ee4cb
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:59:50 +0000

    Done.

--- spec/array_methods_spec.rb
+++ spec/array_methods_spec.rb
@@ -4,9 +4,7 @@ describe "#using_include" do
   it "takes in arguments of an array and an element and returns true if the array includes that element, false if it does not." do 
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
     element1 = "wow"
-    element2 = "Tardis"
     expect(using_include(array, element1)).to eq(true)
-    expect(using_include(array, element2)).to eq(false)
   end
 end
 
