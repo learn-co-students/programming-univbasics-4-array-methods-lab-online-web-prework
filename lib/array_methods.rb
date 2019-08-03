1532d5035ecd2a1ce88f49208cd70af1941e2da5
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-03 22:51:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,13 @@
 
 describe "using_include"
   it "takes in arguments of an array and an element and returns true if the array includes that element, false if it does not."
-    array = ["wow,"]
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    element1 = "wow"
+    element2 = "tardis"
+    expect (using_include(array,element1)).to eq('true')
+    expect (using_include(array,element2)).to eq('false')
+  end
+end
 
 end
 
