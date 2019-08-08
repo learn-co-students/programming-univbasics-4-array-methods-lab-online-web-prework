b15e6df73c6d69c940e11f85bb9952cbe2fc7e78
Tanya Bihag <tbihag@gmail.com>
2019-08-08 15:50:39 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
+  array.include? (element)
+end
+
+def using_sort [array, tanya, love]
+  sorted_cats = using_sort.sort
 
 end
 
-def using_sort(array)
+def using_reverse [array, tanya, love]
+  using_reverse.reverse
 
 end
 
-def using_reverse(array)
+def using_first [array, tanya, love]
+  using_first.first
 
 end
 
-def using_first(array)
+def using_last [array, tanya, love]
+  using_last.last
 
 end
 
-def using_last(array)
-
-end
-
-def using_size(array)
+def using_size [array, tanya, love]
+  using_size.size
 
 end
