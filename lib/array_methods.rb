ae09127f674aa95ce7fa61c0d78eab5a263248ba
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-08 05:08:02 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,43 @@
 def using_include(array, element)
 
+animals = ["cat", "dog", "bird"]
+animals include?("horse")
+  => false
+animals include?("cat")
+  => true
 end
 
 def using_sort(array)
 
+animals = ["cat", "dog", "bird"]
+animals.sort
+  => ["dog", "bird", "cat"]
 end
 
 def using_reverse(array)
 
+animals = ["cat", "dog", "bird"]
+famous_wizards.reverse
+  => ["bird", "dog", "cat"]
 end
 
 def using_first(array)
 
+animals = ["cat", "dog", "bird"]
+animals.first
+  => "cat"
 end
 
 def using_last(array)
 
+animals = ["cat", "dog", "bird"]
+famous_cats.last
+  => "bird"
 end
 
 def using_size(array)
 
+animals = ["cat", "dog", "bird"]
+animals.size
+  => 3
 end
