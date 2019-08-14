0bfd1a29bd7c3b78e43fb657e321a87fade3a7be
Jalysa Reason <jalysareason@gmail.com>
2019-08-14 01:26:08 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,25 @@
 def using_include(array, element)
-
+  array = 
+    element1 =
+    element2 = 
 end
 
 def using_sort(array)
-
+array =
 end
 
 def using_reverse(array)
-
+array = 
 end
 
 def using_first(array)
-
+array =
 end
 
 def using_last(array)
-
+array =
 end
 
 def using_size(array)
-
+array =
 end
