4c1802af4f960fd8bc22d180bef7df00c92925f8
Shamel Lakin <christlife4u@gmail.com>
2019-08-14 21:00:39 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
-def using_include(array, element)
-
+def using_include = [array, element]
+  using_include.include?(element)
 end
 
-def using_sort(array)
-
+def using_sort = [array]
+ using_sort.sort
 end
 
-def using_reverse(array)
-
+def using_reverse = [array]
+  using_reverse.reverse
 end
 
-def using_first(array)
-
+def using_first = [array]
+  using_first.first
 end
 
-def using_last(array)
-
+def using_last = [array]
+  using_last.last
 end
 
-def using_size(array)
-
+def using_size = [array]
+  using_size.size
 end
