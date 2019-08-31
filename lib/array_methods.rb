b7e39781edc82549a9abe86dc21702bacef98ea7
Ruel Tingling <rvtingling@yahoo.com>
2019-08-31 09:38:52 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
-def using_include(array, element)
-
+def using_include(array, element) = ["john", "roger", "mark", "tony"]
+using_include(array, element).include?("john")
 end
 
-def using_sort(array)
-
+def using_sort(array) =  ["john", "roger", "mark", "tony"]
+using_sort(array).sort
 end
 
-def using_reverse(array)
-
+def using_reverse(array) =  ["john", "roger", "mark", "tony"]
+  using_reverse(array).reverse
 end
 
-def using_first(array)
+def using_first(array) =  ["john", "roger", "mark", "tony"]
+  using_first(array).first
+end 
 
+def using_last(array) =  ["john", "roger", "mark", "tony"]
+    using_last(array).last
 end
 
-def using_last(array)
-
-end
-
-def using_size(array)
-
+def using_size(array) =  ["john", "roger", "mark", "tony"]
+    using_size(array).size
 end
