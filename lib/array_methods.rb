d66f5a5eda9894ef6a5a1549bcfeeaf775dd8913
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-08 14:16:04 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,11 @@
 def using_include(array,element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include? (element)
+  element1 = "wow"
+  element2 = "Tardis"
 end
+
+  array.include? (element1)
+  array.include? (element2)
 
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
