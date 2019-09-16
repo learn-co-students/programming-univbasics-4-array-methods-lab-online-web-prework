4f3b434cda7d7890d2735dab186d55cc005816b0
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-16 06:48:20 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,60 @@
 def using_include(array, element)
 
+ def using_include (array), (element)
+ Array.include? (element)
+
+
+  def using_include(array, element)
+
+    def using_include (array), (element)
+    Array.include? (element)
+
+  end
+
+
+  def using_include(array, element)
+
+    def using_include (array), (element)
+    Array.include? (element)
+
+  end
+
 end
 
 def using_sort(array)
+
+def elements.sort (array)
 
 end
 
 def using_reverse(array)
 
+def .reverse (array)
+
+end
 end
 
 def using_first(array)
+
+def movies = "spiderman","Boondocks","Batman"
+
+  movies. first = 'spoderman"
+
 
 end
 
 def using_last(array)
 
+movies .last "Batman"
+
+
 end
 
 def using_size(array)
+
+def movies = ["spiderman","Boondocks","Batman"]
+
+  movies.size
+    => 3
 
 end
