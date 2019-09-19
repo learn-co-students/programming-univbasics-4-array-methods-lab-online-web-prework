abbe2a113bcd4b81ab533c8edbb0aa96ae121bbd
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-19 02:53:24 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+p array = array.first
 end
 
 def using_last(array)
-
+p array = array.last
 end
 
 def using_size(array)
-
+p array = array.length
 end
