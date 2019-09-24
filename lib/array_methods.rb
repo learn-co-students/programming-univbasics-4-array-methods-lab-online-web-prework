e6f105a4bdda4f458202eab01d92d2fd1c866ce8
Katelynne Clizer <kateclizer@gmail.com>
2019-09-24 18:02:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+clizer_fam = ["wow", "Kate", "I", "Judson"]
+clizer_fam.sort
 end
 
 def using_reverse(array)
-
+clizer_fam = ["wow", "Kate", "I", "arrays!"]
+clizer_fam.reverse
 end
 
 def using_first(array)
-
+clizer_fam = ["wow", "Kate", "I", "arrays!"]
+clizer_fam.first
 end
 
 def using_last(array)
-
+clizer_fam = ["wow", "Kate", "I", "arrays!"]
+clizer_fam.last
 end
 
 def using_size(array)
-
+clizer_fam = ["wow", "Kate", "I", "arrays!", "Judson", "Kyle"]
+clizer_fam.size
 end
