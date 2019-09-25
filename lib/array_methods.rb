115038ee28e9206097760afcc5551634695096bc
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-25 01:03:45 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,16 @@
 def using_include(array, element)
-
+famous_singers = ["beyonce", "rihanna", "shakira", "kehlani"]
+  famous_singers.include? ("taylor swift") => true
+    famous_singers.include? ("beyonce") => true
 end
 
 def using_sort(array)
-
+famous_singers.sort
+  =>  ["rihanna", "beyonce", "kehlani", "shakira"]
 end
 
 def using_reverse(array)
-
+famous_singers = ["kehlani", "shakira", "rihanna", "beyonce"]
 end
 
 def using_first(array)
