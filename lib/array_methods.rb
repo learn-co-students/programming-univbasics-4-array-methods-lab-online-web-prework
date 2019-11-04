d750829953e7140ad7cc78d4a2a32cea72c48a3d
Kelly Small <xcxkelly98@gmail.com>
2019-11-04 02:58:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,24 @@
 def using_include(array, element)
-
+Types_of_candy = ["skittles", 'Laffy taffy', "MandM"]
 end
 
 def using_sort(array)
-
+Types_of_candy.sort!
 end
 
 def using_reverse(array)
-
+Types_of_candy.reverse!
 end
 
 def using_first(array)
-
+Types_of_candy.first
 end
 
 def using_last(array)
-
+Types_of_candy.last
 end
 
-def using_size(array)
 
+def using_size(array)
+Types_of_candy.size
 end
