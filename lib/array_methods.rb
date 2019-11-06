494f941c8ec49760690c6910cb5409e58975451e
Emilee Sudweeks <emilee.sudweeks@wework.com>
2019-11-06 00:36:47 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,15 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort!
 end
 
 def using_reverse(array)
-
+  it "takes in argument of an array and and returns the size, or length, of the array" do 
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_size(array)).to eq(6)
 end
 
 def using_first(array)
