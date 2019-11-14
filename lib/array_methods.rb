edd77b683c99f06948dccb9dc06b8fdd24f3eb37
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-14 14:47:48 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
-def using_include(array, element)
-
+def using_include("red", "yellow", "blue", blue)
+array.include? ("blue")
 end
 
 def using_sort(array)
-
+using_include.sort 
 end
 
 def using_reverse(array)
-
+using_include.reverse 
 end
 
 def using_first(array)
-
+using_include.first 
 end
 
 def using_last(array)
-
+using_include.last 
 end
 
 def using_size(array)
-
+using_include.size 
 end
