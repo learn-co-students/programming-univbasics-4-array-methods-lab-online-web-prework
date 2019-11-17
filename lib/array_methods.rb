c737ae327cb3471c18d74b44432f46cd7c8e4f1b
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-17 21:20:43 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
+array.include?(element)
 
-end
 
 def using_sort(array)
-
+array.sort(element)
 end
 
-def using_reverse(array)
-
+def  using_reverse(array)
+array.reverse(element)
 end
 
 def using_first(array)
-
+array.using_first(element)
 end
 
 def using_last(array)
-
+array.using_last(element)
 end
 
-def using_size(array)
-
+def using_first(element)
+array.using_size(element)
 end
