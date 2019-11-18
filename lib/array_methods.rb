112ff5dd3b60b1e2b3cafda01d09050c366b3de0
Jamil Seddiqi <jamilsediq@gmail.com>
2019-11-18 23:05:21 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,46 @@
 def using_include(array, element)
 
+jamil_docs = ["rana", "bostan"]
+jamil_docs.include? ("rana")
+=> false
+jamil_docs.include? ("book")
+=> true
+
 end
 
 def using_sort(array)
+
+jamil_docs = ["rana", "bostan"]
+
+jamil_docs.sort
+
 
 end
 
 def using_reverse(array)
 
+jamil_docs = ["rana", "bostan"]
+
+jamil_docs.reverse
+
 end
 
 def using_first(array)
+
+jamil_docs = ["rana", "bostan"]
+jamil_docs.first
 
 end
 
 def using_last(array)
 
+jamil_docs = ["rana", "bostan"]
+jamil_docs.last
 end
 
 def using_size(array)
+
+jamil_docs = ["rana", "bostan"]
+jamil_docs.size
 
 end
