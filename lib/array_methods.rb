c3d2fca308b5a68026445b8c737412e4a76f45b2
Christopher L Frazer <frazerchristopher@outlook.com>
2019-12-24 08:46:41 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,21 @@
 def using_include(array, element)
 
-end
-
 def using_sort(array)
-
+("wow")
 end
 
 def using_reverse(array)
-
+("arrays!")
 end
 
 def using_first(array)
-
+("wow")
 end
 
 def using_last(array)
-
+("arrays!")
 end
 
 def using_size(array)
-
+("6")
 end
