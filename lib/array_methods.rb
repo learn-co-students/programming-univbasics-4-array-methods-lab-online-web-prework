0409b19d2a1c8811fbd1b42ae9aecb5ca17fa159
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-21 16:37:28 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,5 @@
 def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.include? ['a']
-    array.include? ['wow']
+    array.include? (element)
     
 end
 
